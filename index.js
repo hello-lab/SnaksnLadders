c1f91546034fba257d675bcab5ec9f1843324fbe
ezio <58252733+hello-lab@users.noreply.github.com>
2025-10-25 11:58:35 +0530

    Redirect root to game route instead of serving HTML

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const snakes = { 16: 6, 47: 26, 49: 11, 56: 53, 62: 19, 64: 60, 87: 24, 93: 73, 
 const ladders = { 1: 38, 4: 14, 9: 31, 21: 42, 28: 84, 36: 44, 51: 67, 71: 91, 80: 100 };
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/app/index.html'));
+   // res.sendFile(path.join(__dirname, '/app/index.html'));
+    res.redirect("/game")
 });
 
 app.get('/game', (req, res) => {
